91543f43b6a4e2fc5b8becb8f02848e28a1f4d94
him <himorithm@gmail.com>
2020-04-18 23:12:52 +0530

    Changed alacritty config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,1 +1,14 @@
+
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --hwdec=vdpau --force-window yes {hint-url}')
